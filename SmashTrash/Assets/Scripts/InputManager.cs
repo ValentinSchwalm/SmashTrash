dcf963ea26aa8399227190e683c24e786cf00821
valloho <veronika.schramadei.haber@stud.fh-campuswien.ac.at>
2022-12-30 18:50:24 +0100

    InputManager Update

--- SmashTrash/Assets/Scripts/InputManager.cs
+++ SmashTrash/Assets/Scripts/InputManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class HandGesture : MonoBehaviour
 {
+    public ManoGestureContinuous continuousGesture;
+    private GestureInfo gesture; // the current hand gesture being made
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,35 @@ public class HandGesture : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gesture = ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info;
+
+        // RecognizeInteraction(gesture);
+        // RecignizeSuck(gesture);
+
+    }
+
+    
+  
+    void RecognizeInteraction(GestureInfo gesture)
+    {
+        // Checks if the current visable hand performs a gesture from pinch family
+        // if so, then the code will be executed in this case the phone will vibrate.
+
+        if (gesture.mano_class == ManoClass.PINCH_GESTURE)
+        {
+            // Your code here
+            Handheld.Vibrate();
+        }
+    }
+
+    void RecignizeSuck(GestureInfo gesture)
+    {
+
+        // Method that will run while the selected ManoGestureContinuous is performed.
+
+        if (gesture.mano_gesture_continuous == continuousGesture)
+        {
+            // Code that will execute while continuous gesture is performed
+        }
     }
 }
