64b2824f96f833f9758afa7acb47836efea76956
Nico Schulterer <91744414+Nico-Schulterer@users.noreply.github.com>
2023-01-21 20:05:34 +0100

    A lot

--- SmashTrash/Assets/Scripts/Enemies/Spitter.cs
+++ SmashTrash/Assets/Scripts/Enemies/Spitter.cs
@@ -10,8 +10,15 @@ public class Spitter : Enemy
     [SerializeField] private float patrollingDistance;
     [SerializeField] private float rangeToAttack;
     [SerializeField] private float patrollingRange;
+    //[SerializeField] private GameObject spitWeapon;
+    [SerializeField] private Projectile spit;
+    [SerializeField] private Transform spitPos;
+    [SerializeField] private float MaxThrowForce;
+    [SerializeField] private float throwHeight;
     private Rigidbody rb;
+    private Rigidbody spitRB;
     private Vector3 nextPos;
+    private float Timer = 0;
 
     enum EnemyState
     {
@@ -53,10 +60,40 @@ public class Spitter : Enemy
         rb.velocity = (nextPos - this.transform.position).normalized * movementSpeed;
         //print("Patrolling");
     }
+
+
+
+    private void throwSpit()
+    {
+        if (rangeToAttack > Vector3.Distance(transform.position, distanceToTarget.position) && Timer <= 0)
+        {   
+            Projectile Spit = Instantiate(spit, spitPos.position, Quaternion.identity);
+            //Vector3 throwDirection = (distanceToTarget.position - spitPos.position).normalized;
+            Vector3 displacement = new Vector3(distanceToTarget.position.x, spitPos.position.y, distanceToTarget.position.z) - spitPos.position;
+            float deltaY = distanceToTarget.position.y - spitPos.position.y;
+            float deltaXZ = displacement.magnitude;
+            print("deltaxZ:" + deltaXZ);
+            print("deltaY" + deltaY);
+            
+            float gravity = Spit.Gravity;
+            float throwStrength = Mathf.Clamp(Mathf.Sqrt(gravity * (deltaY + Mathf.Sqrt(Mathf.Pow(deltaY, 2) + Mathf.Pow(deltaXZ, 2)))), 0.01f, MaxThrowForce);
+            float angle = Mathf.PI / 2f - (0.5f * (Mathf.PI / 2 - (deltaY / deltaXZ)));
+            Vector3 initialVelocity = Mathf.Cos(angle) * throwStrength * displacement.normalized + Mathf.Sign(angle) * throwStrength * Vector3.up;
+
+            //throwDirection.y = throwHeight;
+            
+            Spit.InitiateProjectile(damage, initialVelocity);
+            Timer = 3f;
+            print("poop");
+        }
+        Timer -= Time.deltaTime;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        spitRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -73,6 +110,8 @@ public class Spitter : Enemy
             default:
                 break;
         }
+        throwSpit();
+        this.transform.rotation = Quaternion.LookRotation(this.transform.position - distanceToTarget.transform.position);
     }
     private void OnDrawGizmos()
     {
